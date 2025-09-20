df4b674277f793693c142febf4d0615fba2c73be
AyushPathak4650 <ayushpathak4650@gmail.com>
2025-09-20 18:31:17 +0530

    Baseline course defined

---
+++ src/courses/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class AccessRequirement(models.TextChoices):
+    ANYONE = "any", "Anyone"
+    EMAIL_REQUIRED = "email_required", "Email Required"
+
+class PublishedStatus(models.TextChoices):
+    PUBLISHED = "pub", "Published"
+    COMING_SOON = "soon", "Coming Soon"
+    DRAFT = "draft", "Draft"
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    # Image
+    access = models.CharField(max_length=10, choices=AccessRequirement.choices, default=AccessRequirement.EMAIL_REQUIRED)
+    status = models.CharField(max_length=10, choices=PublishedStatus.choices, default=PublishedStatus.DRAFT)
+    
+    @property
+    def is_published(self):
+        return self.status == PublishedStatus.PUBLISHED
