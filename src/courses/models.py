ff5b4e02d9a748dd48279d910c129c9b2d3c1301
AyushPathak4650 <ayushpathak4650@gmail.com>
2025-10-18 13:56:23 +0530

    baseline course designed

---
+++ src/courses/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class AccessRequirement(models.TextChoices):
+    ANYONE = "any", "Anyone"
+    EMAIL_REQUIRED = "email", "Email Required"
+
+class PublishedStatus(models.TextChoices):
+    PUBLISHED = "publish", "Published"
+    DRAFT = "draft", "Draft"
+    COMING_SOON = "soon", "Coming Soon"
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(null=True, blank=True)
+    
+    status = models.CharField(max_length=10, choices=AccessRequirement.choices, default=AccessRequirement.EMAIL_REQUIRED)
+    
+    status = models.CharField(max_length=10, choices=PublishedStatus.choices, default=PublishedStatus.DRAFT)
+    
+    #image
