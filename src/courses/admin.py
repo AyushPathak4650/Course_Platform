c8e0d5ba9ec75ebe5b40377563c2fedf876f4ffc
AyushPathak4650 <ayushpathak4650@gmail.com>
2025-10-26 19:37:26 +0530

    added width field to images in admin page

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from cloudinary import CloudinaryImage
 from .models import Course
 # Register your models here.
 
@@ -16,5 +17,7 @@ class CourseAdmin(admin.ModelAdmin):
     
     def display_image(self, obj, *args, **kwargs):
         url = obj.image.url
+        img_id = str(obj.image)
+        cloudinary_html = CloudinaryImage(img_id).image(width = 250)
         
-        return format_html(f"<img src='{url}' />")
+        return format_html(cloudinary_html)
